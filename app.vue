0874b4d369fdfebf444a18ef598c60fac7d44945
Elvis <elviskb@gmail.com>
2023-06-27 16:20:45 +0300

    modified the scrollbar

--- app.vue
+++ app.vue
@@ -107,3 +107,27 @@ const locoScroll = ref()
     </div>
   </KeepAlive>
 </template>
+
+<style>
+/* Works on Firefox */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #64748b #111827;
+}
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #64748b;
+  border-radius: 60px;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #111827;
+  border-radius: 20px;
+  border: 3px solid #111827;
+}
+</style>
