40dfe3a01d21179a82316b18da8ecd9bcd6a3865
Elvis <elviskb@gmail.com>
2024-08-02 16:54:11 +0300

    added new portfolio items

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { gsap } from 'gsap';
+import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
 import redcross from '~/assets/images/protfolio/redcross-emblem.svg'
@@ -11,67 +11,83 @@ import tradewinds from '~/assets/images/protfolio/tradewinds.png'
 import ca from '~/assets/images/protfolio/CA_Logo.png'
 import safariCollection from '~/assets/images/protfolio/The-Safari-Collection.jpg'
 import tatuCity from '~/assets/images/protfolio/tatu-city.jpg'
+import jms from '~/assets/images/protfolio/JointMedicalStores.png'
+import kimfay from '~/assets/images/protfolio/KimfayLogo.png'
+import montis from '~/assets/images/protfolio/MontisLogo.png'
 
-definePageMeta({
-
-})
+definePageMeta({})
 
 const portfolio = ref([
   {
+    company: 'Joint Medical Stores',
+    service: 'Health Facility Mapping and Route Optimization',
+    logoUrl: jms,
+  },
+  {
+    company: 'Montis Homes',
+    service: 'Tenancy Management Solution',
+    logoUrl: montis,
+  },
+  {
+    company: 'KIMFAY',
+    service: 'Azure Cloud Server Optimization',
+    logoUrl: kimfay,
+  },
+  {
     company: 'Communications Authority of Kenya',
     service: 'Digital Trunked Radio Networks Framework Project',
-    logoUrl: ca
+    logoUrl: ca,
   },
   {
     company: 'Tatu City',
     service: 'Performance 365 Implementation',
-    logoUrl: tatuCity
+    logoUrl: tatuCity,
   },
   {
     company: 'Kenya Electricity Transmission Company',
     service: 'Visitor Management System',
-    logoUrl: ketraco
+    logoUrl: ketraco,
   },
   {
     company: 'The Safari Collection',
     service: 'SharePoint Intranet Portal',
-    logoUrl: safariCollection
+    logoUrl: safariCollection,
   },
   {
     company: 'Communication Authority',
     service: 'Digital Access Gap Study',
-    logoUrl: ca
+    logoUrl: ca,
   },
   {
     company: 'Kenya Red Cross',
     service: 'Informal Settlement Mapping in Nairobi County',
-    logoUrl: redcross
+    logoUrl: redcross,
   },
   {
     company: 'KETRACO',
     service: 'Integrated Location Intelligence System (ILIS)',
-    logoUrl: ketraco
+    logoUrl: ketraco,
   },
   {
     company: 'Dawa Limited',
     service: 'HR Intranet and Performance Appraisal Automation',
-    logoUrl: dawaLtd
+    logoUrl: dawaLtd,
   },
   {
     company: 'Victoria Commercial Bank',
     service: 'SharePoint Intranet with Company Pulse Solution',
-    logoUrl: victoriaBank
+    logoUrl: victoriaBank,
   },
   {
     company: 'Kenya Red Cross',
     service: 'Dadaab Refugee Camp Drone Image Processing',
-    logoUrl: redcross
+    logoUrl: redcross,
   },
   {
     company: 'Tradewinds Aviation',
     service: 'Intranet DMS and Flight Tracking Solution',
-    logoUrl: tradewinds
-  }
+    logoUrl: tradewinds,
+  },
 ])
 
 const contxt = ref()
@@ -90,57 +106,54 @@ onMounted(() => {
           trigger: item,
           refreshPriority: -1,
           // markers: true
-        }
+        },
       })
 
       let proxy = { skew: 0 }
-      let skewSetter = gsap.quickSetter(item, "skewY", "deg")
+      let skewSetter = gsap.quickSetter(item, 'skewY', 'deg')
       let clamp = gsap.utils.clamp(-30, 30) // Don't let skew go beyond these degrees
 
       // console.log('PROXY: ', proxy);
-
 
       ScrollTrigger.create({
         trigger: item,
         start: '0 100%',
         end: '100% 0',
         onUpdate: (self) => {
-          let skew = clamp(self.getVelocity() / -400);
+          let skew = clamp(self.getVelocity() / -400)
           // console.log('SKEW WITH CLAMP: ', skew);
           if (Math.abs(skew) > Math.abs(proxy.skew)) {
-            proxy.skew = skew;
+            proxy.skew = skew
             gsap.to(proxy, {
               skew: 0,
               duration: 0.6,
               stagger: 0.2,
-              ease: "power2",
+              ease: 'power2',
               overwrite: true,
-              onUpdate: () => skewSetter(proxy.skew)
-            });
+              onUpdate: () => skewSetter(proxy.skew),
+            })
           }
-        }
-      });
-
+        },
+      })
     })
 
     // console.log(digitalText);
 
-    gsap.set(".card", { transformOrigin: "right center", force3D: true })
-
+    gsap.set('.card', { transformOrigin: 'right center', force3D: true })
 
     // usage:
-    batch(".card", {
+    batch('.card', {
       interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
-      batchMax: 3,   // maximum batch size (targets)
-      onEnter: batch => gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-      onLeave: batch => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-      onEnterBack: batch => gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-      onLeaveBack: batch => gsap.set(batch, { autoAlpha: 0, overwrite: true })
+      batchMax: 3, // maximum batch size (targets)
+      onEnter: (batch) =>
+        gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
+      onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+      onEnterBack: (batch) =>
+        gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
+      onLeaveBack: (batch) =>
+        gsap.set(batch, { autoAlpha: 0, overwrite: true }),
       // you can also define things like start, end, etc.
-    });
-
-
-
+    })
 
     // the magical helper function (no longer necessary in GSAP 3.3.1 because it was added as ScrollTrigger.batch())...
     function batch(targets, vars) {
@@ -148,25 +161,33 @@ onMounted(() => {
         interval = vars.interval || 0.1,
         proxyCallback = (type, callback) => {
           let batch = [],
-            delay = gsap.delayedCall(interval, () => { callback(batch); batch.length = 0; }).pause();
-          return self => {
-            batch.length || delay.restart(true);
-            batch.push(self.trigger);
-            vars.batchMax && vars.batchMax <= batch.length && delay.progress(1);
-          };
+            delay = gsap
+              .delayedCall(interval, () => {
+                callback(batch)
+                batch.length = 0
+              })
+              .pause()
+          return (self) => {
+            batch.length || delay.restart(true)
+            batch.push(self.trigger)
+            vars.batchMax && vars.batchMax <= batch.length && delay.progress(1)
+          }
         },
-        p;
+        p
       for (p in vars) {
-        varsCopy[p] = (~p.indexOf("Enter") || ~p.indexOf("Leave")) ? proxyCallback(p, vars[p]) : vars[p];
+        varsCopy[p] =
+          ~p.indexOf('Enter') || ~p.indexOf('Leave')
+            ? proxyCallback(p, vars[p])
+            : vars[p]
       }
-      gsap.utils.toArray(targets).forEach(target => {
-        let config = {};
+      gsap.utils.toArray(targets).forEach((target) => {
+        let config = {}
         for (p in varsCopy) {
-          config[p] = varsCopy[p];
+          config[p] = varsCopy[p]
         }
-        config.trigger = target;
-        ScrollTrigger.create(config);
-      });
+        config.trigger = target
+        ScrollTrigger.create(config)
+      })
     }
   })
   // })
@@ -187,21 +208,18 @@ onMounted(() => {
       ease: 'power2.inOut',
       scrub: 1,
       // markers: true
-    }
-
+    },
+  }).to('.image-logo', {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.main-content',
+      start: 'top 30%',
+      end: 'top 0%',
+      // duration: 2,
+      ease: 'power2.inOut',
+      scrub: 1,
+    },
   })
-    .to('.image-logo', {
-      opacity: 0,
-      scrollTrigger: {
-        trigger: '.main-content',
-        start: 'top 30%',
-        end: 'top 0%',
-        // duration: 2,
-        ease: 'power2.inOut',
-        scrub: 1,
-      }
-    })
-
 })
 
 onUnmounted(() => {
@@ -210,7 +228,8 @@ onUnmounted(() => {
 
 useSeoMeta({
   title: 'Portfolio | Aegir Consult',
-  description: 'We are a technology firm that deals in geo-informatics, information systems, system integration and geospatial technologies to both improve business efficacy and management operations',
+  description:
+    'We are a technology firm that deals in geo-informatics, information systems, system integration and geospatial technologies to both improve business efficacy and management operations',
   ogTitle: '[og:title]',
   ogDescription: '[og:description]',
   ogImage: '[og:image]',
@@ -218,61 +237,84 @@ useSeoMeta({
   twitterTitle: '[twitter:title]',
   twitterDescription: '[twitter:description]',
   twitterImage: '[twitter:image]',
-  twitterCard: 'summary'
+  twitterCard: 'summary',
 })
 
 useHead({
   htmlAttrs: {
-    lang: 'en'
+    lang: 'en',
   },
   link: [
     {
       rel: 'icon',
       type: 'image/png',
-      href: '/favicon.ico'
-    }
-  ]
+      href: '/favicon.ico',
+    },
+  ],
 })
 </script>
 
 <template>
   <div class="bg-slate-100">
-    <div class="w-full h-[350px] bg-[#f1f1e6] bg-[url('@/assets/images/services.jpg')] object-bottom py-5 relative">
+    <div
+      class="w-full h-[350px] bg-[#f1f1e6] bg-[url('@/assets/images/services.jpg')] object-bottom py-5 relative"
+    >
       <div class="absolute inset-0 bg-[#000] bg-opacity-20 z-0"></div>
     </div>
 
-    <div class="max-w-4xl mx-auto mt-10 space-y-5 px-7 md:pl-7 md:pr-16 main-content">
+    <div
+      class="max-w-4xl mx-auto mt-10 space-y-5 px-7 md:pl-7 md:pr-16 main-content"
+    >
       <div class="">
-        <h4 class=" small-heading uppercase font-display tracking-widest text-[#df9f1f] font-bold">Our Esteemed Partners
+        <h4
+          class="small-heading uppercase font-display tracking-widest text-[#df9f1f] font-bold"
+        >
+          Our Esteemed Partners
         </h4>
         <h1 class="font-display font-bold text-5xl">Portfolio</h1>
       </div>
-      <div class=" text-xl text-gray-600">
-        <p>
-          Making each of our clients happy, one project at a time.
-        </p>
+      <div class="text-xl text-gray-600">
+        <p>Making each of our clients happy, one project at a time.</p>
       </div>
     </div>
 
-    <div class="max-w-5xl py-10  mx-auto min-h-screen">
-
-
-      <div class="portfolio-section grid md:grid-cols-2 lg:grid-cols-3 gap-5 ml-5 mr-16 mt-10 -z-10">
-        <div v-for="(item, index) in portfolio" :key="index"
-          class="card shadow-sm px-6 py-10 bg-white relative hover:-translate-y-3 transition duration-1000 ease-in-out hover:shadow-xl">
-          <div class=" inset-0 absolute bg-[#213f9a] hover:opacity-90 space-y-4 opacity-0 transition duration-1000
-          ease-in-out px-6 py-4">
-            <div class="text-xs font-display uppercase font-bold tracking-wider text-[#df9f1f]">
+    <div class="max-w-5xl py-10 mx-auto min-h-screen">
+      <div
+        class="portfolio-section grid md:grid-cols-2 lg:grid-cols-3 gap-5 ml-5 mr-16 mt-10 -z-10"
+      >
+        <div
+          v-for="(item, index) in portfolio"
+          :key="index"
+          class="card shadow-sm px-6 py-10 bg-white relative hover:-translate-y-3 transition duration-1000 ease-in-out hover:shadow-xl"
+        >
+          <div
+            class="inset-0 absolute bg-[#213f9a] hover:opacity-90 space-y-4 opacity-0 transition duration-1000 ease-in-out px-6 py-4"
+          >
+            <div
+              class="text-xs font-display uppercase font-bold tracking-wider text-[#df9f1f]"
+            >
               Project
             </div>
             <div class="flex">
-              <p class="text-white font-display font-bold text-2xl hover:opacity-100">
+              <p
+                class="text-white font-display font-bold text-2xl hover:opacity-100"
+              >
                 {{ item.service }}
               </p>
               <div>
-                <svg class="w-12 h-12 text-[#e6ae17] ml-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                <svg
+                  class="w-12 h-12 text-[#e6ae17] ml-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
                 </svg>
               </div>
             </div>
@@ -283,16 +325,24 @@ useHead({
                 </button> -->
           </div>
           <div class="">
-            <img :src="item.logoUrl" class="mx-auto object-contain w-48 h-24" alt="">
+            <img
+              :src="item.logoUrl"
+              class="mx-auto object-contain w-48 h-24"
+              alt=""
+            />
           </div>
           <div class="">
-            <div class="text-xs font-display uppercase font-bold tracking-wider text-[#df9f1f] mt-5">
+            <div
+              class="text-xs font-display uppercase font-bold tracking-wider text-[#df9f1f] mt-5"
+            >
               Company
             </div>
             <div class="text-xl font-display font-bold">
               {{ item.company }}
             </div>
-            <div class="text-xs font-dispaly uppercase font-bold tracking-wider md:hidden text-[#df9f1f] mt-5">
+            <div
+              class="text-xs font-dispaly uppercase font-bold tracking-wider md:hidden text-[#df9f1f] mt-5"
+            >
               Project
             </div>
             <div class="md:hidden">
@@ -342,10 +392,8 @@ useHead({
           </div>
         </div> -->
       </div>
-
     </div>
   </div>
 </template>
-
 
 <style scoped></style>
